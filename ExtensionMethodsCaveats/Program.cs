d9a00f84b1e1cf8bf908eee3c089e5c105d702c5
Kulpot <kulpotwahu@gmail.com>
2023-07-01 19:52:46 +0800

    Add project files.

---
+++ ExtensionMethodsCaveats/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=3E3FTgKBcm0&list=PL90AF0EFFEF782D27&index=4
+// extension methods - are normal static methods
+
+/*class Cow
+{
+    public int numMoos;
+
+}
+
+static class CowMethods
+{
+    public static void Moo(this Cow _this)
+    {
+        
+        _this.numMoos++;
+        Console.WriteLine("Moooo " + _this.numMoos);
+    }
+}
+
+static class MainClass
+{
+    static void Main()
+    {
+        Cow c = new Cow();
+        Cow c2 = new Cow();
+     
+        c2.Moo();
+      
+    }
+}*/
+
+class MainClass
+{
+    static void Main()
+    {
+        System.Linq.Enumerable
+    }
+}
